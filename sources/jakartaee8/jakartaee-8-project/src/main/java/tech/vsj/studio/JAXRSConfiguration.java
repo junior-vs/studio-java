512e668695ee02c2033a951f61f926eecf7bdebc
Valdir Junior <junior_vs@outlook.com>
2021-05-04 10:08:03 -0300

    s: evoluido

--- sources/jakartaee8/jakartaee-8-project/src/main/java/tech/vsj/studio/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package tech.vsj.studio;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
