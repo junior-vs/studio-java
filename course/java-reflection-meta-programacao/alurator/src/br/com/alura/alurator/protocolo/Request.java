339941b27946cc47e152cb4efb235062a5d4e5cb
valdir junior <valdir.junior.dev@gmail.com>
2024-01-30 18:36:53 -0300

    course java-reflection-meta-programacao

---
+++ course/java-reflection-meta-programacao/alurator/src/br/com/alura/alurator/protocolo/Request.java
@@ -0,0 +1,26 @@
+package br.com.alura.alurator.protocolo;
+
+public class Request {
+	
+	private String nomeControle;
+	private String nomeMetodo;
+
+	public Request(String url) {
+		// nomeControle nomeMetodo
+		String[] partesUrl = url.replaceFirst("/", "")
+				.split("/");
+
+		nomeControle = Character.toUpperCase(partesUrl[0].charAt(0)) + 
+						partesUrl[0].substring(1) + "Controller";
+		
+		nomeMetodo = partesUrl[1];
+	}
+
+	public String getNomeControle() {
+		return nomeControle;
+	}
+	
+	public String getNomeMetodo() {
+		return nomeMetodo;
+	}
+}
