85cfbf0cb30b0f56af07f61f002c1ba50a5142c6
Valdir Junior <valdir-souza.junior@caixa.gov.br>
2024-03-04 16:57:38 -0300

    estudo sobre o mutiny

--- framework/studio-mutiny/src/main/java/com/studio/mutiny/UniEx.java
+++ framework/studio-mutiny/src/main/java/com/studio/mutiny/UniEx.java
@@ -1,5 +1,11 @@
 package com.studio.mutiny;
 
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.subscription.UniSubscriber;
+import io.smallrye.mutiny.subscription.UniSubscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Optional;
@@ -11,14 +17,13 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
-import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.subscription.UniSubscriber;
-import io.smallrye.mutiny.subscription.UniSubscription;
 import static java.util.concurrent.CompletableFuture.delayedExecutor;
 
 public class UniEx {
 
-    public static void main(String[] args) throws InterruptedException {
+    private Logger logger = LoggerFactory.getLogger(UniEx.class);
+
+    public void main(String[] args) throws InterruptedException {
         // exemplo1();
         // ex2();
         // ex3();
@@ -32,41 +37,47 @@ public class UniEx {
         // uniFromFailure();
         // uniFromCompleteStage();
         // uniMisc();
-        uniDelay();
+        new UniEx().uniDelay();
 
     }
 
-    private static void uniDelay() {
+    private void uniDelay() {
         Uni.createFrom().item(666)
-                .onItem().delayIt().by(Duration.ofSeconds(1))
-                .subscribe().with(System.out::println);
+                .onItem()
+                .delayIt().by(Duration.ofSeconds(1))
+                .map(String::valueOf)
+                .subscribe().with(logger::info);
 
-        System.out.println("wheit");
+        logger.info("wait...");
 
         Uni.createFrom().item(666)
-                .onItem().delayIt()
-                .until(n -> Uni.createFrom().completionStage(
-                        CompletableFuture.supplyAsync(
-                                () -> "Ok",
+                .onItem()
+                .delayIt().until(n -> Uni.createFrom()
+                        .completionStage(CompletableFuture.supplyAsync(() -> "Ok",
                                 CompletableFuture.delayedExecutor(5, TimeUnit.SECONDS))))
-                .subscribe().with(System.out::println);
+                .map(String::valueOf)
+                .subscribe().with(logger::info);
     }
 
-    private static void uniMisc() {
+    private  void uniMisc() {
         Uni.createFrom().nothing()
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+                .onItem().transform(Object::toString)
+                .subscribe().with(logger::info, failure -> System.out.println(failure.getMessage()));
 
         Uni.createFrom().voidItem()
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+                .onItem().transform(Object::toString)
+                .subscribe().with(logger::info, failure -> System.out.println(failure.getMessage()));
 
         Uni.createFrom().nullItem()
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+                .onItem().transform(Object::toString)
+                .subscribe().with(logger::info, failure -> System.out.println(failure.getMessage()));
 
         Uni.createFrom().optional(Optional.of("Hello"))
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+                .onItem().transform(Object::toString)
+                .subscribe().with(logger::info, failure -> System.out.println(failure.getMessage()));
 
         Uni.createFrom().converter(i -> Uni.createFrom().item("[" + i + "]"), 10)
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+                .subscribe().with(logger::info, failure -> System.out.println(failure.getMessage()));
     }
 
     /**
@@ -77,30 +88,29 @@ public class UniEx {
      * logs the result, and subscribes to it, printing the result or failure
      * message. Finally,
      * it waits for 2 seconds before completing.
-     * 
+     *
      * @throws InterruptedException
      */
-    private static void uniFromCompleteStage() throws InterruptedException {
+    private void uniFromCompleteStage() throws InterruptedException {
 
-        var cs = CompletableFuture
-                .supplyAsync(() -> "Hello!", delayedExecutor(3, TimeUnit.SECONDS))
-
+        var cs = CompletableFuture.supplyAsync(() -> "Hello!", delayedExecutor(3, TimeUnit.SECONDS))
                 .thenApply(String::toUpperCase);
 
-        Uni.createFrom().completionStage(cs).log()
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+        Uni.createFrom()
+                .completionStage(cs).log()
+                .subscribe().with(logger::info, failure -> System.out.println(failure.getMessage()));
 
         Thread.sleep(4000);
     }
 
-    private static void exemplo1() {
+    private void exemplo1() {
         Uni.createFrom().item("hello")
                 .onItem().transform(item -> item + " mutiny")
                 .onItem().transform(String::toUpperCase)
-                .subscribe().with(item -> System.out.println(">> " + item));
+                .subscribe().with(item -> logger.info(">> " + item));
     }
 
-    private static void ex2() {
+    private void ex2() {
         Uni<String> uni1 = Uni.createFrom().item("hello");
         Uni<String> uni2 = uni1.onItem().transform(item -> item + " mutiny");
         Uni<String> uni3 = uni2.onItem().transform(String::toUpperCase);
@@ -108,14 +118,14 @@ public class UniEx {
         uni3.subscribe().with(item -> System.out.println(">> " + item));
     }
 
-    private static void ex3() {
+    private void ex3() {
         Uni<String> uni = Uni.createFrom().item("hello");
         uni.onItem().transform(item -> item + " mutiny");
         uni.onItem().transform(String::toUpperCase);
         // uni.subscribe().with(item -> System.out.println(">> " + item));
     }
 
-    private static void ex4() {
+    private void ex4() {
         Uni<String> uni = Uni.createFrom().item("Hello, world!");
 
         uni.subscribe().withSubscriber(new UniSubscriber<String>() {
@@ -136,43 +146,45 @@ public class UniEx {
         });
     }
 
-    private static void exUniFromSupplier() {
+    private void exUniFromSupplier() {
         Random random = new Random();
         Uni<Integer> uniFromSupplier = Uni.createFrom().item(random::nextInt);
         for (var i = 0; i < 5; i++) {
-            uniFromSupplier.subscribe().with(System.out::println);
+            uniFromSupplier
+                    .map(String::valueOf)
+                    .subscribe().with(logger::info);
         }
     }
 
-    private static void ex5() {
+    private void ex5() {
         Uni.createFrom().item(1)
-                .onItem().transform(i -> "hello-" + i)
-                .onItem().delayIt().by(Duration.ofMillis(1000))
-                .subscribe().with(System.out::println);
+                .onItem().transform(i -> "hello-" + i).onItem()
+                .delayIt().by(Duration.ofMillis(1000))
+                .subscribe().with(logger::info);
     }
 
-    private static void uniFromSupplierAndState() {
-        Uni<Integer> uniFromSupplierAndState = Uni.createFrom()
-                .item(AtomicInteger::new, i -> i.addAndGet(10));
+    private void uniFromSupplierAndState() {
+        Uni<Integer> uniFromSupplierAndState = Uni.createFrom().item(AtomicInteger::new, i -> i.addAndGet(10));
 
         for (var i = 0; i < 5; i++) {
-            uniFromSupplierAndState.subscribe().with(System.out::println);
+            uniFromSupplierAndState
+                    .map(String::valueOf)
+                    .subscribe().with(logger::info);
         }
     }
 
     /**
      * A description of the entire Java function.
      *
-     * @param paramName description of parameter
      * @return description of return value
      */
 
-    private static void uniDeferred() {
+    private void uniDeferred() {
         AtomicLong ids = new AtomicLong();
         Uni<Long> deferredUni = Uni.createFrom().deferred(() -> Uni.createFrom().item(ids::incrementAndGet));
 
         for (var i = 0; i < 5; i++) {
-            deferredUni.subscribe().with(System.out::println);
+            deferredUni.map(String::valueOf).subscribe().with(logger::info);
         }
     }
 
@@ -181,7 +193,7 @@ public class UniEx {
      * to it,
      * printing the emitted value. It throws InterruptedException.
      */
-    private static void uniFromEmitter() throws InterruptedException {
+    private void uniFromEmitter() throws InterruptedException {
 
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
         CountDownLatch emitterLatch = new CountDownLatch(1);
@@ -193,34 +205,32 @@ public class UniEx {
             });
         });
 
-        uniFromEmitter.subscribe().with(System.out::println);
+        uniFromEmitter.subscribe().with(logger::info);
 
         emitterLatch.await();
     }
 
     /**
      * Generates a Uni from an emitter and state, and subscribes to it 5 times.
-     *
      */
-    private static void uniFromEmitterAndState() {
+    private void uniFromEmitterAndState() {
 
-        Uni<Integer> uniFromEmitterAndState = Uni.createFrom()
-                .emitter(AtomicInteger::new, (i, e) -> e.complete(i.addAndGet(10)));
+        Uni<Integer> uniFromEmitterAndState = Uni.createFrom().emitter(AtomicInteger::new, (i, e) -> e.complete(i.addAndGet(10)));
 
         for (var i = 0; i < 5; i++) {
-            uniFromEmitterAndState.subscribe().with(System.out::println);
+            uniFromEmitterAndState .map(String::valueOf).subscribe().with(logger::info);
         }
     }
 
-    private static void uniFromFailure() {
+    private void uniFromFailure() {
 
-        Uni.createFrom()
-                .failure(new IOException("Boom"))
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+        Uni.createFrom().failure(new IOException("Boom"))
+                .map(String::valueOf)
+                .subscribe().with(logger::info, failure -> logger.error(failure.getMessage()));
 
-        Uni.createFrom()
-                .failure(() -> new IOException("Badaboom"))
-                .subscribe().with(System.out::println, failure -> System.out.println(failure.getMessage()));
+        Uni.createFrom().failure(() -> new IOException("Badaboom"))
+                .map(String::valueOf)
+                .subscribe().with(logger::info, failure -> logger.error(failure.getMessage()));
     }
 
 }
